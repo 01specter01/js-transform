0ef2182d2a20460abf0d0b68e931ba9644aee7f0
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-04 14:51:28 +0200

    test

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const text = "  Lorem Ipsum Dolor sit amet.    ";
+// const text = "  Lorem Ipsum Dolor sit amet.    ";
 
-console.log(text);
-console.log(text.toLocaleLowerCase());
-console.log(text.toLocaleUpperCase());
+// console.log(text);
+// console.log(text.toLocaleLowerCase());
+// console.log(text.toLocaleUpperCase());
 
-console.log(text.length);
+// console.log(text.length);
 
-console.log(text.substring(2, 13));
-console.log(text.substring(14));
+// console.log(text.substring(2, 13));
+// console.log(text.substring(14));
 
-console.log(text.trim().length); // keine lehrzeichen vorne hinten
+// console.log(text.trim().length); // keine lehrzeichen vorne hinten
 
-console.log(text.trim().length, text.trim().toLocaleUpperCase());
+// console.log(text.trim().length, text.trim().toLocaleUpperCase());
 
-const trimmedText = text.trim();
-console.log(trimmedText.length);
-console.log(trimmedText.toLocaleUpperCase());
+// const trimmedText = text.trim();
+// console.log(trimmedText.length);
+// console.log(trimmedText.toLocaleUpperCase());
+
+// const text = "Lorem Iopsahhh Leo ghjdkjaj gdwj.";
+// const searchTerm = "Leo";
+// const isInText = text.includes(searchTerm);
+
+// console.log(isInText);
+const hellrot = 120;
+console.log(hellrot.toString(16));
